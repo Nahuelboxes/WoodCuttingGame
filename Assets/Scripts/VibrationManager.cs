0399331479265d7ed7d72ab59a0d549a9091f1cf
Federico Speranza <fedde.speranza@hotmail.com>
2020-10-31 23:34:43 -0300

    changes for vib manager and dotween asset
    cuidado

--- Assets/Scripts/VibrationManager.cs
+++ Assets/Scripts/VibrationManager.cs
@@ -1,51 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
+using System;
 public class VibrationManager : MonoBehaviour
 {
+    #region Variables
+        public bool enableVibration = true;
+        public GameObject switchBtn;
+
+    #endregion
     #region Singleton
     public static VibrationManager instance;
-    void AwakeSingleton(){
-        if (instance == null){
+    void AwakeSingleton()
+    {
+        if (instance == null)
+        {
             instance = this;
-        }else{
+        }
+        else
+        {
             Destroy(this);
         }
     }
-  #endregion
+    #endregion
 
-    void Awake(){
+    void Awake()
+    {
         AwakeSingleton();
     }
 
-    public void Vibrate(vibrationDuration vibdur){
-    Vibration.Vibrate(ConvertoMs(vibdur));
-    }
-  
-    private long ConvertoMs(vibrationDuration vibdur){
-    switch (vibdur)
+    public void Vibrate(vibrationDuration vibdur)
     {
-        case vibrationDuration.small:
-        return 100;
-        break;
-         case vibrationDuration.medium:
-        return 350;
-        break;
-         case vibrationDuration.large:
-        return 800;
-        break;
-         case vibrationDuration.xlarge:
-        return 1200;
-        break;
-
-        default:
-        return 100;
-    }
+        if (enableVibration)
+        {
+            Vibration.Vibrate(ConvertoMs(vibdur));
+        }
     }
 
-    void OnDestroy(){
+    private long ConvertoMs(vibrationDuration vibdur)
+    {
+        switch (vibdur)
+        {
+            case vibrationDuration.small:
+                return 100;
+                break;
+            case vibrationDuration.medium:
+                return 350;
+                break;
+            case vibrationDuration.large:
+                return 800;
+                break;
+            case vibrationDuration.xlarge:
+                return 1200;
+                break;
+
+            default:
+                return 100;
+        }
+    }
+
+    void OnDestroy()
+    {
         Vibration.Cancel();
-    }                              
+    }
+
+   public void OnToggleVibration(){
+        int statesign;
+        switchBtn.transform.DOLocalMoveX(-switchBtn.transform.localPosition.x, 0.2f);
+        statesign = Math.Sign(-switchBtn.transform.localPosition.x);
+         Debug.Log("el estado, " + statesign );
+         if (statesign == 1){
+             enableVibration = true;
+         }else{
+             enableVibration = false;
+         }
+    }
 }
 public enum vibrationDuration {small, medium, large, xlarge,}
